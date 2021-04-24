076d96e7526dd6f1b0715d5b58f029c02b1d68a8
Darwin Luque <darwin.luque.98@gmail.com>
2021-04-24 14:06:36 -0600

    add a screen for creating new meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is the first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is the second meetup',
+  },
+  {
+    id: 'm3',
+    title: 'A Third Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is the thir meetup',
+  },
+]
+
 const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <MeetupList meetups={DUMMY_MEETUPS} />
     </div>
   );
 };
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (meetupData) => {
+    console.log(meetupData);
+  };
+
+  return (
+    <div>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </div>
+  );
+};
+
+export default NewMeetupPage;
