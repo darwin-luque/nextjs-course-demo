5ddd2f9c956682d7b85ca248952a3bb53f4eeba4
Darwin Luque <darwin.luque.98@gmail.com>
2021-04-25 11:18:52 -0600

    feat: add new features to online db and prepare the get method for the api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,12 +2,45 @@ import { MongoClient } from 'mongodb'
 
 const methods = {};
 
-methods.post = (req, res) => {
-  MongoClient.connect('mongodb+srv://admin-darwin:<password>@cluster0.zixju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+methods.post = async (body, collection, res) => {
+  try {
+    const result = await collection.insertOne(body.data);
+    console.log(result);
+    res.status(201).json({ message: 'Meetup succesfully created' });
+  } catch {
+    res.status(502).json({ message: 'Could not create the meetup' });
+  }
 };
 
-const handler = (req, res) => {
-  methods[req.method](req, res);
+methods.get = async (body, collection, res) => {
+  const getAll = typeof body.data.getAll === 'boolean' ? body.data.getAll : true;
+  if (getAll) {
+    try {
+      const meetups = collection.find().toArray();
+      res.status(201).json(meetups);
+    } catch {
+      res.status(502).json({ message: 'Could not get the meetups'});
+    }
+  }
+};
+
+const handler = async (req, res) => {
+  const { method, body } = req;
+  try {
+    const client = await MongoClient.connect(
+      'mongodb+srv://admin-darwin:LLMc1JLYb6hN5dNr@cluster0.zixju.mongodb.net/meetups?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
+    const db = client.db();
+    const meetupCollection = db.collection('meetups');
+    await methods[method.trim().toLowerCase()](body, meetupCollection, res);
+    client.close();
+  } catch {
+    res.status(502).json({ message: 'Could not connect with the server' });
+  }
 };
 
 export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meetupData) => {
-    console.log(meetupData);
+  const router = useRouter();
+  const addMeetupHandler = async (meetupData) => {
+    const res = await fetch('/api/new-meetup', {
+      method: 'post',
+      body: JSON.stringify({ data: meetupData }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (res.status === 201) {
+      router.push('/');
+    } else {
+      alert('Could not create the meetup');
+    }
   };
 
   return (
